e7b96a6fd800547f1edd75f0f38fd3cf33c7dc21
Daniel Kistemacher <danielkistemacher@gmail.com>
2022-10-11 15:28:50 -0300

    Update 11_10

--- ProjetoIntegrador/src/pi/ref323070/univates/ProjetoIntegrador.java
+++ ProjetoIntegrador/src/pi/ref323070/univates/ProjetoIntegrador.java
@@ -16,7 +16,8 @@ public class ProjetoIntegrador {
         System.out.println("3. Editar");
         System.out.println("4. Excluir");
         System.out.println("5. Cadastrar empréstimo");
-        System.out.println("6. Sair");
+        System.out.println("6. Cadastrar manutenção");
+        System.out.println("7. Sair");
         System.out.println("Digite o número da opção: ");
 
         opcaoEscolhida = input.nextInt();
@@ -32,7 +33,9 @@ public class ProjetoIntegrador {
                 break;
             case 5: System.out.println("Não implementado!");
                 break;      
-            case 6: break;
+            case 6: System.out.println("Não implementado!");
+                break;
+            case 7: break;
             default: 
                     System.out.println("Opção inválida!");
                     menuPrincipal();
@@ -47,25 +50,22 @@ public class ProjetoIntegrador {
         System.out.println("1. Incluir Pessoa");
         System.out.println("2. Incluir Tipos de objeto");
         System.out.println("3. Incluir Objeto");
-        System.out.println("4. Incluir Manutenção");
-        System.out.println("5. Voltar ao menu principal");
-        System.out.println("6. Sair");
+        System.out.println("4. Voltar ao menu principal");
+        System.out.println("5. Sair");
         System.out.println("Digite o número da opção: ");
 
         opcaoEscolhida = input.nextInt();
 
         switch (opcaoEscolhida){
-            /* case 1: incluirPessoa();
+            case 1: /*incluirPessoa();*/ System.out.println("Não implementado!");
                     break;
-            case 2: incluirTipoObjeto();
+            case 2: /*incluirTipoObjeto();*/ System.out.println("Não implementado!");
                     break;
-            case 3: incluirObjeto();
+            case 3: /*incluirObjeto();*/ System.out.println("Não implementado!");
                     break; 
-            case 4: incluirManutencao();
-                    break; */
-            case 5: menuPrincipal();
+            case 4: menuPrincipal();
                     break;
-            case 6: break;
+            case 5: break;
             default: 
                 System.out.println("Opção inválida!");
                 menuIncluir();
@@ -77,28 +77,30 @@ public class ProjetoIntegrador {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("1. Consultar Pessoas");
-        System.out.println("2. Consultar Tipos de objetos");
-        System.out.println("3. Consultar Objetos");
-        System.out.println("4. Consultar Manutenções");
-        System.out.println("5. Voltar ao menu principal");
-        System.out.println("6. Sair");
+        System.out.println("1. Consultar pessoas");
+        System.out.println("2. Consultar tipos de objetos");
+        System.out.println("3. Consultar objetos");
+        System.out.println("4. Consultar manutenções");
+        System.out.println("5. Consultar empréstimos");
+        System.out.println("6. Voltar ao menu principal");
+        System.out.println("7. Sair");
         System.out.println("Digite o número da opção: ");
 
         opcaoEscolhida = input.nextInt();
 
         switch (opcaoEscolhida){
-            /* case 1: consultaPessoa();
+            case 1: /*consultaPessoa();*/ System.out.println("Não implementado!");
                 break;
-            case 2: consultaTipoObjeto();
+            case 2: /*consultaTipoObjeto();*/ System.out.println("Não implementado!");
                 break;
-            case 3: consultaObjeto();
+            case 3: /*consultaObjeto();*/ System.out.println("Não implementado!");
                 break;
-            case 4: consultaManutencao();
-                break; */
-            case 5: menuPrincipal();
+            case 4: /*consultaManutencao();*/ System.out.println("Não implementado!");
                 break;
-            case 6: break;
+            case 5: /*consultaEmprestimo();*/ System.out.println("Não implementado!");
+            case 6: menuPrincipal();
+                break;
+            case 7: break;
             default: 
                 System.out.println("Opção inválida!");
                 menuConsultar();
@@ -111,27 +113,30 @@ public class ProjetoIntegrador {
         Scanner input = new Scanner(System.in);
 
         System.out.println("1. Excluir Pessoas");
-        System.out.println("2. Excluir Tipos de objetos");
-        System.out.println("3. Excluir Objetos");
-        System.out.println("4. Excluir Manutenções");
-        System.out.println("5. Voltar ao menu principal");
-        System.out.println("6. Sair");
+        System.out.println("2. Excluir tipos de objetos");
+        System.out.println("3. Excluir objetos");
+        System.out.println("4. Excluir manutenções");
+        System.out.println("5. Excluir empréstimos");
+        System.out.println("6. Voltar ao menu principal");
+        System.out.println("7. Sair");
         System.out.println("Digite o número da opção: ");
 
         opcaoEscolhida = input.nextInt();
 
         switch (opcaoEscolhida){
-            /* case 1: excluirPessoa();
+            case 1: /*excluirPessoa();*/ System.out.println("Não implementado!");
                 break;
-            case 2: excluirTipoObjeto();
+            case 2: /*excluirTipoObjeto();*/ System.out.println("Não implementado!");
                 break;
-            case 3: excluirObjeto();
+            case 3: /*excluirObjeto();*/ System.out.println("Não implementado!");
                 break;
-            case 4: excluirManutencao();
-                break; */
-            case 5: menuPrincipal();
+            case 4: /*excluirManutencao();*/ System.out.println("Não implementado!");
                 break;
-            case 6: break;
+            case 5: /*excluirEmprestimo();*/ System.out.println("Não implementado!");
+                break;
+            case 6: menuPrincipal();
+                break;
+            case 7: break;
             default: 
                 System.out.println("Opção inválida!");
                 menuExcluir();
@@ -143,28 +148,29 @@ public class ProjetoIntegrador {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("1. Editar Pessoas");
-        System.out.println("2. Editar Tipos de objetos");
-        System.out.println("3. Editar Objetos");
-        System.out.println("4. Editar Manutenções");
-        System.out.println("5. Voltar ao menu principal");
-        System.out.println("6. Sair");
+        System.out.println("1. Editar pessoas");
+        System.out.println("2. Editar tipos de objetos");
+        System.out.println("3. Editar objetos");
+        System.out.println("4. Editar manutenções");
+        System.out.println("5. Editar empréstimos");
+        System.out.println("6. Voltar ao menu principal");
+        System.out.println("7. Sair");
         System.out.println("Digite o número da opção: ");
 
         opcaoEscolhida = input.nextInt();
 
         switch (opcaoEscolhida){
-            /* case 1: editarPessoa();
+            case 1: /*editarPessoa();*/System.out.println("Não implementado!");
                 break;
-            case 2: editarTipoObjeto();
+            case 2: /*editarTipoObjeto();*/System.out.println("Não implementado!");
                 break;
-            case 3: editarObjeto();
+            case 3: /*editarObjeto();*/System.out.println("Não implementado!");
                 break;
-            case 4: editarManutencao();
-                break; */
-            case 5: menuPrincipal();
+            case 4: /*editarManutencao();*/System.out.println("Não implementado!");
                 break;
-            case 6: break;
+            case 6: menuPrincipal();
+                break;
+            case 7: break;
             default: 
                 System.out.println("Opção inválida!");
                 menuEditar();
