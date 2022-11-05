5a7d4e2bbe08426b63f4dd87be7ef26a59398b18
Daniel Kistemacher <danielkistemacher@gmail.com>
2022-11-05 16:20:46 -0300

    update

--- ProjetoIntegrador/src/pi/ref323070/univates/ProjetoIntegradorMatriz.java
+++ ProjetoIntegrador/src/pi/ref323070/univates/ProjetoIntegradorMatriz.java
@@ -6,6 +6,7 @@ public class ProjetoIntegradorMatriz {
     static String pessoas[][] = new String[3][2];
     static String tiposObjeto[] = new String[3];
     static String objetos[][] = new String[3][3];
+    static String manutObjetos[][] = new String[3][3];
 
     public static void main(String[] args) {
         inicializaCadastros();
@@ -27,6 +28,12 @@ public class ProjetoIntegradorMatriz {
         for(int x=0; x<3; x++){
             for (int y=0; y<3; y++){
                 objetos[x][y] = "";
+            }
+        }
+        //inicializa Manutenção de Objetos
+        for(int x=0; x<3; x++){
+            for (int y=0; y<3; y++){
+                manutObjetos[x][y] = "";
             }
         }
     }
@@ -59,8 +66,8 @@ public class ProjetoIntegradorMatriz {
 
     public static void menuPrincipal() {
         
-        Scanner menuPrincipal = new Scanner(System.in);
-        int opcaoEscolhida;
+        Scanner input = new Scanner(System.in);
+        int opcaoEscolhida, linha;
 
         System.out.println("\n SISTEMA DE EMPRÉSTIMO DE OBJETOS");
         System.out.println("1. Incluir");
@@ -72,7 +79,7 @@ public class ProjetoIntegradorMatriz {
         System.out.println("7. Sair");
         System.out.print("Digite o número da opção: ");
 
-        opcaoEscolhida = menuPrincipal.nextInt();
+        opcaoEscolhida = input.nextInt();
 
         switch (opcaoEscolhida) {
             case 1:
@@ -91,7 +98,20 @@ public class ProjetoIntegradorMatriz {
                 // cadastrarEmprestimo();
                 break;
             case 6:
-                // cadastrarManutencao();
+                linha = espacoLivreMatriz(manutObjetos);
+                    if (linha == -1) { //se a posicao retornada for -1 e porque nao ha mais espaco
+                        System.out.println("Não há espaços para novos cadastros! \n");
+                    } else {
+                        input.nextLine();
+                        System.out.print("Nome do objeto: ");
+                        manutObjetos[linha][0] = input.nextLine().toUpperCase();
+                        System.out.print("Descrição da manut.: ");
+                        manutObjetos[linha][1] = input.nextLine().toUpperCase();
+                        System.out.print("Status: ");
+                        manutObjetos[linha][2] = input.nextLine().toUpperCase();
+
+                        System.out.println("Objeto cadastrado com sucesso! \n");
+                    }
                 break;
             case 7: System.exit(0);
                 break;
@@ -131,8 +151,8 @@ public class ProjetoIntegradorMatriz {
                         pessoas[linha][1] = input.nextLine().toUpperCase();
 
                         System.out.println("Cliente cadastrado com sucesso! \n");
-                        break;
                     }
+                    break;
                 case 2:
                     linha = espacoLivreVetor(tiposObjeto);
                     if (linha == -1) { //se a posicao retornada for -1 e porque nao ha mais espaco
@@ -147,7 +167,20 @@ public class ProjetoIntegradorMatriz {
                     }
                     break;
                 case 3:
-                    // incluirObjeto();
+                    linha = espacoLivreMatriz(objetos);
+                    if (linha == -1) { //se a posicao retornada for -1 e porque nao ha mais espaco
+                        System.out.println("Não há espaços para novos cadastros! \n");
+                    } else {
+                        input.nextLine();
+                        System.out.print("Nome do objeto: ");
+                        objetos[linha][0] = input.nextLine().toUpperCase();
+                        System.out.print("Tipo de objeto: ");
+                        objetos[linha][1] = input.nextLine().toUpperCase();
+                        System.out.print("Situação do objeto: ");
+                        objetos[linha][2] = input.nextLine().toUpperCase();
+
+                        System.out.println("Objeto cadastrado com sucesso! \n");
+                    }
                     break;
                 case 4: menuPrincipal();
                     break;
@@ -206,7 +239,14 @@ public class ProjetoIntegradorMatriz {
                     // consultarEmprestimo();
                     break;
             case 5:
-                    // consultarManutencao();
+                    for (int x=0; x<3; x++){
+                    System.out.println("Código: " + x);
+                    System.out.println("Nome objeto: " + manutObjetos[x][0]);
+                    System.out.println("Descrição da manut.: " + manutObjetos[x][1]);
+                    System.out.println("Status: " + manutObjetos[x][2]);
+                    System.out.println("==============");
+                }
+                menuPrincipal();
                     break;
             case 6: menuPrincipal();
                     break;
